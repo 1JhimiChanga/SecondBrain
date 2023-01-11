5b5fbfdc35f16b516054052486a69ecc19f8e03c
Jimmy Van <jvan709@gmail.com>
2023-01-11 14:10:28 -0700

    DirFix: Fixing initial setup with index.js, index.css and reportWebVitals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Screens
-import MainPage from "./Screens/index"
+import MainPage from "./Screens/main"
 
 
 function App() {
